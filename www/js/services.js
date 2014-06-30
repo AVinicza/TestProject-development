14d22c383320e78bfb7d01648c3e398e5dcabab1
Viniczai Attila <attila.viniczai@hotmail.com>
2014-06-30 16:50:23 +0200

    Revert "Revert "Please enter the commit message for your changes. Lines starting""
    This reverts commit eefa1ec9f6d751649c78d0decbed5bb22c29cb01.

--- www/js/services.js
+++ www/js/services.js
@@ -4,23 +4,31 @@ angular.module('starter.services', [])
  * A simple example service that returns some data.
  */
 .factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
+	var friends = [];  
 
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
+  var fields = ['*'];
+  var options = {
+  	filter: "",
+  	multiple: true
+  };
+  
+  navigator.contacts.find( fields, function(contacts){
+  	friends = contacts;
+  }, function(error){
+  	console.log("Error");
+  }, options);
 
   return {
     all: function() {
       return friends;
     },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
+    get: function(phoneNumber) {
+      for (var i=0; i < contacts.length; i++) {
+      	if(friends[i] == phoneNumber){
+      		return friends[i];
+      	}
+		return "";
+      };
     }
-  }
+  };
 });
